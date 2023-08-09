a5f753fc0df7d659f8dea4d47ea7073f7f322dd3
SongGayoung <xjongam1x@gmail.com>
2023-08-09 12:14:24 +0900

    배열 추가

--- section02/practice.js
+++ section02/practice.js
@@ -1,10 +1,3 @@
-const gazero = {
-  name: "가영",
-  age: 19,
-  say: function () {
-    console.log(`헤이 모두들 안녕! 내가 누군지 아늬! ${this.name}이다 !`);
-  },
-};
+let arr = [1, 2, 3, 4, 5];
 
-console.log("name" in gazero);
-console.log("gender" in gazero);
+console.log(arr.length);
