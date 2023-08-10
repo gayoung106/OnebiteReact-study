c8f3ad47dc12d8d065b22727e3a472460b0bb364
SongGayoung <xjongam1x@gmail.com>
2023-08-10 09:18:51 +0900

    반복문

--- section02/practice.js
+++ section02/practice.js
@@ -1,3 +1,12 @@
-let arr = [1, 2, 3, 4, 5];
+let gazero = {
+  name: "가영",
+  age: 19,
+  gender: "female",
+};
 
-console.log(arr.length);
+const gazeroValues = Object.values(gazero);
+console.log(gazeroValues);
+
+for (let i = 0; i < gazeroValues.length; i++) {
+  console.log(gazeroValues[i]);
+}
